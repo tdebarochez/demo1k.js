82b4499618275a3e3dec994fd52be001eac41c30
Thomas Debarochez <thomas.barochez+github@gmail.com>
2010-08-11 22:21:06 +0200

    initial commit

---
+++ demo1k.js
@@ -0,0 +1,47 @@
+ca = document.getElementById('ca'),
+c = ca.getContext('2d'),
+h = 400,
+wi = 800,
+M = Math,
+r = M.random, 
+ro = M.round,
+ph = " js1k.com",
+pl = 9,
+s = wi / 40,
+w = s * 2,
+a = [],
+p = [],
+co = 'rgba(150,250,150,0.8)',
+m = h / 2,
+l = 100;
+ca.width = wi,
+ca.height = h;
+while(l--) a.push(new (function() {
+  var x = ro(r() * s) * w,
+  y = r() * h,
+  tc = ph[ro(r() * (pl - 1))];
+  this.d = function () {
+    (y += s) > h + w && (x = ro(r() * s) * w) && (y = 0);
+    c.fillStyle = co;
+    c.fillText(tc, x-w, y);
+    ph[x / w - 1] == tc && y == m && p.indexOf(tc) == -1 && p.push(tc);
+  }
+}));
+t = setInterval(function () {
+  la = a.length, lp = p.length;
+  c.fillStyle = 'rgba(0,0,0,.4)';
+  c.rect(0, 0, wi, h);
+  c.fill();
+  while (la--) a[la].d();
+  if (lp == pl) {
+    a.shift();
+    if (a.length == 0) {
+      setTimeout("clearTimeout(t)", 300);
+    }
+  }
+  while (lp--) {
+    c.fillStyle = co;
+    c.fillText(p[lp], ph.indexOf(p[lp]) * w, m);
+  }
+}, 50);
+c.font = w+"px Arial";
