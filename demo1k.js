9ff5896b735b68af27cf92ba75bc122cf5932738
Thomas Debarochez <thomas.barochez+github@gmail.com>
2010-08-12 23:50:48 +0200

    some enhancements - spotlight changed by spiral - remove text prompt, use static text - add gradient on final text - add mouse gesture

--- demo1k.js
+++ demo1k.js
@@ -1,63 +1,66 @@
-ca = document.getElementById('c'),
-c = ca.getContext('2d'),
+d = document,
+C = d.getElementById('c'),
+c = C.getContext('2d'),
 h = 400,
 u = 800,
 M = Math,
 r = M.random, 
 e = M.round,
-mp = M.pow,
-mc = M.cos,
-ms = M.sin,
-pi = M.PI,
-ph = prompt('text ?', " js1k.com"),
-pl = ph.length,
+P = M.pow,
+H = " js1k.com",
 s = u / 40,
 w = s * 2,
 a = [],
 p = [],
 m = h / 2,
-l = 99,
-bx = 400,
-by = 200,
-br = 99,
-dr = r() * pi * 2,
-ca.width = u,
-ca.height = h;
+l = 50,
+X = h,
+Y = 200,
+k = 0,
+C.width = u,
+C.height = h
 while(l--) a.push(new (function() {
   var x = e(r() * s) * w,
   y = r() * h,
-  tc = ph[e(r() * (pl - 1))];
+  C = H[e(r() * 8)];
   this.d = function () {
-    (y += s) > h + w && (x = e(r() * s) * w) && (y = 0);
-    c.fillText(tc, x-w, y);
-    ph[x / w - 1] == tc && y == m && p.indexOf(tc) == -1 && p.push(tc);
+    (y += s) > h + w && (x = e(r() * s) * w) & (y = 0);
+    c.fillText(C, x-w, y);
+    H[x / w - 1] == C && y == m && p.indexOf(C) < 0 && G < 9 && p.push(C);
   }
 }));
 t = setInterval(function () {
-  la = a.length, lp = p.length;
+  B = a.length, G = p.length;
   c.fillStyle = 'rgba(0,0,0,.5)';
   c.rect(0, 0, u, h);
   c.fill();
-    c.fillStyle = 'rgba(150,250,150,0.8)';
-  while (la--) a[la].d();
-    if (lp == pl && a.length > 0)
-        a.shift();
-  while (lp--)
-      c.fillText(p[lp], ph.indexOf(p[lp]) * w, m);
+  c.fillStyle = W;
+  G == 9 && B > 0 && a.shift() && B--
+  while (B--) a[B].d()
+  c.fillStyle = g;
+  while (G--)
+    c.fillText(p[G], H.indexOf(p[G]) * w, m)
   i = c.getImageData(0,0,u,h);
   z = i.data;
   l = z.length;
-    (bx > u-br || bx < br) && (dr = - dr + pi);
-    (by > h-br || by < br) && (dr = 2 * pi - dr);
-    bx += 9*mc(dr); by += 9*ms(dr);
+  k += 2
   while (l-=4) {
-      dt = M.sqrt(mp(bx - (l/4)% u, 2) + mp(by - M.floor(l / 4 / u), 2));
-      if (dt < br) {
-          z[l]   = br - z[l] - dt;
-          z[l+1] = br - z[l+1] - dt;
-          z[l+2] = br - z[l+2] - dt; 
-      }
+    D = M.sqrt(P(X - (l/4)% u, 2) + P(Y - M.floor(l / 4 / u), 2)) ;
+    b = 99 - D;
+    if (0 < b) {
+      n = M.sin((D - k%25) / 4);
+      z[l] = (b - z[l]) * n;
+      z[l+1] = (b - z[l+1]) * n;
+      z[l+2] = (b - z[l+2]) * n
+    }
   }
-    c.putImageData(i, 0, 0);
-}, 50);
+  c.putImageData(i, 0, 0);
+}, 80);
 c.font = w+"px Arial";
+d.onmousemove = function (e) { X = e.clientX; Y = e.clientY }
+g = c.createLinearGradient(0, 180, 0, m);
+g.b = g.addColorStop;
+g.b(0, W = '#8f8');
+g.b(.5, '#efe');
+g.b(.5, W);
+g.b(1, '#afa');
