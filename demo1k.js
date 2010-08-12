0de898f0c74eb4561d33cdeee1572081a39794c5
Thomas Debarochez <thomas.barochez+github@gmail.com>
2010-08-12 15:39:40 +0200

    add spotlight + optimize

--- demo1k.js
+++ demo1k.js
@@ -1,47 +1,63 @@
 ca = document.getElementById('c'),
 c = ca.getContext('2d'),
 h = 400,
-wi = 800,
+u = 800,
 M = Math,
 r = M.random, 
-ro = M.round,
+e = M.round,
+mp = M.pow,
+mc = M.cos,
+ms = M.sin,
+pi = M.PI,
 ph = prompt('text ?', " js1k.com"),
 pl = ph.length,
-s = wi / 40,
+s = u / 40,
 w = s * 2,
 a = [],
 p = [],
-co = 'rgba(150,250,150,0.8)',
 m = h / 2,
-l = 100;
-ca.width = wi,
+l = 99,
+bx = 400,
+by = 200,
+br = 99,
+dr = r() * pi * 2,
+ca.width = u,
 ca.height = h;
 while(l--) a.push(new (function() {
-  var x = ro(r() * s) * w,
+  var x = e(r() * s) * w,
   y = r() * h,
-  tc = ph[ro(r() * (pl - 1))];
+  tc = ph[e(r() * (pl - 1))];
   this.d = function () {
-    (y += s) > h + w && (x = ro(r() * s) * w) && (y = 0);
-    c.fillStyle = co;
+    (y += s) > h + w && (x = e(r() * s) * w) && (y = 0);
     c.fillText(tc, x-w, y);
     ph[x / w - 1] == tc && y == m && p.indexOf(tc) == -1 && p.push(tc);
   }
 }));
 t = setInterval(function () {
   la = a.length, lp = p.length;
-  c.fillStyle = 'rgba(0,0,0,.4)';
-  c.rect(0, 0, wi, h);
+  c.fillStyle = 'rgba(0,0,0,.5)';
+  c.rect(0, 0, u, h);
   c.fill();
+    c.fillStyle = 'rgba(150,250,150,0.8)';
   while (la--) a[la].d();
-  if (lp == pl) {
-    a.shift();
-    if (a.length == 0) {
-      setTimeout("clearTimeout(t)", 300);
-    }
+    if (lp == pl && a.length > 0)
+        a.shift();
+  while (lp--)
+      c.fillText(p[lp], ph.indexOf(p[lp]) * w, m);
+  i = c.getImageData(0,0,u,h);
+  z = i.data;
+  l = z.length;
+    (bx > u-br || bx < br) && (dr = - dr + pi);
+    (by > h-br || by < br) && (dr = 2 * pi - dr);
+    bx += 9*mc(dr); by += 9*ms(dr);
+  while (l-=4) {
+      dt = M.sqrt(mp(bx - (l/4)% u, 2) + mp(by - M.floor(l / 4 / u), 2));
+      if (dt < br) {
+          z[l]   = br - z[l] - dt;
+          z[l+1] = br - z[l+1] - dt;
+          z[l+2] = br - z[l+2] - dt; 
+      }
   }
-  while (lp--) {
-    c.fillStyle = co;
-    c.fillText(p[lp], ph.indexOf(p[lp]) * w, m);
-  }
+    c.putImageData(i, 0, 0);
 }, 50);
 c.font = w+"px Arial";
